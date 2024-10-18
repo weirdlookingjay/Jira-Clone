1fd49cb8200be73901f5dbf166e2a28e28600b76
Joe Clark <weirdlookingjay@live.com>
2024-10-18 14:07:06 -0400

    created top menu and mobile sidebar

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,5 +1,4 @@
 import { getCurrent } from "@/features/auth/actions";
-import { UserButton } from "@/features/auth/components/user-button";
 import { redirect } from "next/navigation";
 
 export default async function Home() {
@@ -7,6 +6,6 @@ export default async function Home() {
 
   if (!user) redirect("/sign-in");
   return (
-    <UserButton />
+    <div>This is a home page</div>
   );
 }
