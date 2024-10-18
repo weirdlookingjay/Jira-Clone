58e9abe65a43708c9ab39f82dbfee2abb04de79d
Joe Clark <weirdlookingjay@live.com>
2024-10-18 12:08:24 -0400

    created sidebar navigation

---
+++ src/components/sidebar.tsx
@@ -0,0 +1,19 @@
+import Image from "next/image"
+import Link from "next/link"
+import { DottedSeperator } from "./dotted-seperator"
+import { Navigation } from "./navigation"
+
+
+export const Sidebar = () => {
+    return (
+        <aside className="h-full bg-neutral-100 p-4 w-full">
+            <Link href="/">
+                <Image src="/projects-hub.svg" alt="logo" width={164} height={48} />
+            </Link>
+            <DottedSeperator className="my-4" />
+            <Navigation />
+        </aside>
+    )
+}
+
+export default Sidebar
