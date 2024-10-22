d2793ce30459ca6171b399be4daa571c9ce09c11
Joe Clark <weirdlookingjay@live.com>
2024-10-22 11:27:47 -0400

    Implemented image upload in the create workspace form

--- src/features/workspaces/components/create-workspace-form.tsx
+++ src/features/workspaces/components/create-workspace-form.tsx
@@ -1,15 +1,27 @@
 "use client";
 
+import { useRef } from "react";
+
 import { zodResolver } from "@hookform/resolvers/zod";
 import { useForm } from "react-hook-form";
 import { createWorkspaceShema } from "../schemas";
-import { z } from "zod"
+import { z } from "zod";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { DottedSeperator } from "@/components/dotted-seperator";
-import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import {
+    Form,
+    FormControl,
+    FormField,
+    FormItem,
+    FormLabel,
+    FormMessage,
+} from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { useCreateWorkspace } from "../api/use-create-workspace";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
+import Image from "next/image";
+import { ImageIcon } from "lucide-react";
 
 interface CerateWorkspaceFormProps {
     onCancel?: () => void;
@@ -17,16 +29,29 @@ interface CerateWorkspaceFormProps {
 
 export const CreateWorkspaceForm = ({ onCancel }: CerateWorkspaceFormProps) => {
     const { mutate, isPending } = useCreateWorkspace();
+    const inputRef = useRef<HTMLInputElement>(null);
 
     const form = useForm<z.infer<typeof createWorkspaceShema>>({
         resolver: zodResolver(createWorkspaceShema),
         defaultValues: {
-            name: ""
-        }
+            name: "",
+        },
     });
 
     const onSubmit = (values: z.infer<typeof createWorkspaceShema>) => {
-        mutate({ json: values });
+        const finalValues = {
+            ...values,
+            image: values.image instanceof File ? values.image : "",
+        };
+        mutate({ form: finalValues });
+    }
+
+
+    const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+        const file = e.target.files?.[0];
+        if (file) {
+            form.setValue("image", file);
+        }
     };
 
     return (
@@ -43,24 +68,92 @@ export const CreateWorkspaceForm = ({ onCancel }: CerateWorkspaceFormProps) => {
                 <Form {...form}>
                     <form onSubmit={form.handleSubmit(onSubmit)}>
                         <div className="flex flex-col gap-y-4">
-                            <FormField control={form.control} name="name" render={({ field }) => (
-                                <FormItem>
-                                    <FormLabel>Workspace name</FormLabel>
-                                    <FormControl>
-                                        <Input {...field} placeholder="Enter workspace name" />
-                                    </FormControl>
-                                    <FormMessage />
-                                </FormItem>
-                            )} />
+                            <FormField
+                                control={form.control}
+                                name="name"
+                                render={({ field }) => (
+                                    <FormItem>
+                                        <FormLabel>Workspace name</FormLabel>
+                                        <FormControl>
+                                            <Input {...field} placeholder="Enter workspace name" />
+                                        </FormControl>
+                                        <FormMessage />
+                                    </FormItem>
+                                )}
+                            />
+                            <FormField
+                                control={form.control}
+                                name="image"
+                                render={({ field }) => (
+                                    <div className="flex flex-col gap-y-2">
+                                        <div className="flex items-center gap-x-5">
+                                            {field.value ? (
+                                                <div className="size-[72px] relative rounded-md overflow-hidden">
+                                                    <Image
+                                                        alt="Logo"
+                                                        fill
+                                                        className="object-cover"
+                                                        src={
+                                                            field.value instanceof File
+                                                                ? URL.createObjectURL(field.value)
+                                                                : field.value
+                                                        }
+                                                    />
+                                                </div>
+                                            ) : (
+                                                <Avatar className="size-[72px]">
+                                                    <AvatarFallback>
+                                                        <ImageIcon className="size-[36px] text-neutral-400" />
+                                                    </AvatarFallback>
+                                                </Avatar>
+                                            )}
+                                            <div className="flex flex-col">
+                                                <p className="text-sm">Workspace Icon</p>
+                                                <p className="text-sm text-muted-foreground">
+                                                    JPG, PNG, SVG or JPEG, max 1mb
+                                                </p>
+                                                <input
+                                                    type="file"
+                                                    className="hidden"
+                                                    accept=".jpg,.png,.jpeg,.svg"
+                                                    ref={inputRef}
+                                                    onChange={handleImageChange}
+                                                    disabled={isPending}
+                                                />
+                                                <Button
+                                                    type="button"
+                                                    disabled={isPending}
+                                                    variant="teritary"
+                                                    size="xs"
+                                                    className="w-fill"
+                                                    onClick={() => inputRef.current?.click()}
+                                                >
+                                                    Upload Image
+                                                </Button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )}
+                            />
                         </div>
                         <DottedSeperator className="py-7" />
                         <div className="flex items-center justify-between">
-                            <Button type="button" variant="secondary" size="lg" disabled={isPending} onClick={onCancel}>Cancel</Button>
-                            <Button type="submit" size="lg" disabled={isPending}>Create Workspace</Button>
+                            <Button
+                                type="button"
+                                variant="secondary"
+                                size="lg"
+                                disabled={isPending}
+                                onClick={onCancel}
+                            >
+                                Cancel
+                            </Button>
+                            <Button type="submit" size="lg" disabled={isPending}>
+                                Create Workspace
+                            </Button>
                         </div>
                     </form>
                 </Form>
             </CardContent>
         </Card>
-    )
-}
+    );
+};
--- src/features/workspaces/server/route.ts
+++ src/features/workspaces/server/route.ts
@@ -2,30 +2,55 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { createWorkspaceShema } from "../schemas";
 import { sessionMiddleware } from "@/lib/session-middleware";
-import { DATABASE_ID, WORKSPACES_ID } from "@/config";
+import { DATABASE_ID, IMAGES_BUCKET_ID, WORKSPACES_ID } from "@/config";
 import { ID } from "node-appwrite";
 
 const app = new Hono().post(
   "/",
-  zValidator("json", createWorkspaceShema),
+  zValidator("form", createWorkspaceShema),
   sessionMiddleware,
   async (c) => {
     const databases = c.get("databases");
+    const storage = c.get("storage");
     const user = c.get("user");
 
-    const { name } = c.req.valid("json");
+    const { name, image } = c.req.valid("form");
 
-    const workspace = await databases.createDocument(
-      DATABASE_ID,
-      WORKSPACES_ID,
-      ID.unique(),
-      {
-        name,
-        userId: user.$id,
-      }
-    );
+    let uploadedImageUrl: string | undefined;
 
-    return c.json({ data: workspace });
+    if (image instanceof File) {
+      const file = await storage.createFile(
+        IMAGES_BUCKET_ID,
+        ID.unique(),
+        image
+      );
+
+      const arrayBuffer = await storage.getFilePreview(
+        IMAGES_BUCKET_ID,
+        file.$id
+      );
+
+      uploadedImageUrl = `data:image/png;base64,${Buffer.from(
+        arrayBuffer
+      ).toString("base64")}`;
+    }
+
+    try {
+      const workspace = await databases.createDocument(
+        DATABASE_ID,
+        WORKSPACES_ID,
+        ID.unique(),
+        {
+          name,
+          userId: user.$id,
+          imageUrl: uploadedImageUrl,
+        }
+      );
+      return c.json({ data: workspace });
+    } catch (error) {
+      console.error("Error creating workspace:", error);
+      return c.json({ error: "Failed to create workspace" }, 500);
+    }
   }
 );
 
