d3f46d267394a244a2191d7650ae2bf1115a8b95
Joe Clark <weirdlookingjay@live.com>
2024-10-22 16:00:36 -0400

    Updated WorkspaceSwitcher so when a value is selected from the list it remains with the url

--- src/components/workspace-switcher.tsx
+++ src/components/workspace-switcher.tsx
@@ -5,9 +5,17 @@ import { RiAddCircleFill } from "react-icons/ri"
 import { useGetWorkspaces } from "@/features/workspaces/api/use-get-workspaces";
 import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "./ui/select";
 import { WorkspaceAvatar } from "@/features/workspaces/components/workspace-avatar";
+import { useRouter } from "next/navigation";
+import { useWorkspaceId } from "@/features/workspaces/hooks/use-workspace-id";
 
 export const WorkspaceSwitcher = () => {
+  const workspaceId = useWorkspaceId();
+  const router = useRouter();
   const { data: workspaces } = useGetWorkspaces();
+
+  const onSelect = (id: string) => {
+    router.push(`/workspaces/${id}`);
+  }
 
   return (
     <div className="flex flex-col gap-y-2">
@@ -15,7 +23,7 @@ export const WorkspaceSwitcher = () => {
         <p className="text-xs uppercase text-neutral-500">Workspaces</p>
         <RiAddCircleFill className="size-5 text-neutral-500 cursor-pointer hover:opacity-75 transition" />
       </div>
-      <Select>
+      <Select onValueChange={onSelect} value={workspaceId}>
         <SelectTrigger className="w-full bg-neutral-200 font-medium p-1">
           <SelectValue placeholder="No workspace selected" />
         </SelectTrigger>
